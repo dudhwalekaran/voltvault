3e53d93ae033affe8c2114681ef1a6cbb39caa95
dudhwalekaran <dudhwalekaran@gmail.com>
2025-06-12 16:55:38 +0530

    Update Vsc-hvdc-link.js

--- models/Vsc-hvdc-link.js
+++ models/Vsc-hvdc-link.js
@@ -1,8 +1,14 @@
-// models/Bus.js
 import mongoose from 'mongoose';
 
 const VscSchema = new mongoose.Schema({
-  vsc: { type: String, required: true },
+  vsc: {
+    type: Number,
+    required: [true, 'VSC value is required'],
+    validate: {
+      validator: Number.isInteger,
+      message: 'VSC must be a valid number'
+    }
+  },
 });
 
 const Vsc = mongoose.models.Vsc || mongoose.model('Vsc', VscSchema);
