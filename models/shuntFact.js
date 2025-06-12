30528181ae05b98d8ce3c8762d78faf70297b962
dudhwalekaran <dudhwalekaran@gmail.com>
2025-06-12 17:10:15 +0530

    Update shuntFact.js

--- models/shuntFact.js
+++ models/shuntFact.js
@@ -1,8 +1,14 @@
-// models/Bus.js
 import mongoose from 'mongoose';
 
 const ShuntSchema = new mongoose.Schema({
-  shunt: { type: String, required: true },
+  shunt: {
+    type: Number,
+    required: [true, 'Shunt value is required'],
+    validate: {
+      validator: Number.isFinite,
+      message: 'Shunt must be a valid number'
+    }
+  },
 });
 
 const ShuntFact = mongoose.models.ShuntFact || mongoose.model('ShuntFact', ShuntSchema);
